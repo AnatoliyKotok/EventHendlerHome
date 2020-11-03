bdfa70e30af28a199098d6a9080d245b9157edc8
AnatoliyKotok <61416716+AnatoliyKotok@users.noreply.github.com>
2020-11-03 12:38:38 +0200

    Add files via upload

---
+++ ClokDelegat/ClokDelegat/Clok.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClokDelegat
+{
+    class Clok
+    {
+        int h;
+        int m;
+        public int H { get; set; }
+        public int M { get; set; }
+        public Clok(int h,int m)
+        {
+            this.H = h;
+            this.M = m;
+        }
+        public void TornOf()
+        {
+            this.H = 0;
+            this.M = 0;
+            MyEventHendler args = new MyEventHendler();
+            args.Massege = $"The alarm clock is torn off";
+            TimeToWakeUp(args);
+        }
+        public void Tick()
+        {
+            this.M++;
+            MyEventHendler args = new MyEventHendler();
+            args.Massege = $"Its time to wake up! {this.H}/{this.M}";
+            TimeToWakeUp(args);
+        }
+        protected virtual void TimeToWakeUp(MyEventHendler e)
+        {
+            EventHandler<MyEventHendler> handler = AlarmClock;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        public event EventHandler<MyEventHendler> AlarmClock;
+    }
+
+    class MyEventHendler : EventArgs
+    {
+        public string Massege { get; set; }
+    }
+}
---
+++ ClokDelegat/ClokDelegat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClokDelegat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clok clok = new Clok(12,43);
+            clok.AlarmClock += c_TimeTowakeUp;
+            //clok.Tick();
+            clok.TornOf();
+        }
+        static void c_TimeTowakeUp(object o, MyEventHendler e)
+        {
+            Console.WriteLine(e.Massege);
+            Environment.Exit(0);
+        }
+    }
+}
